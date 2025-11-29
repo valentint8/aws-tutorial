bbfbf17da2f6f5799ce8467186abc635078bb65e
tgval <todireanugeorge15@gmail.com>
2025-11-29 21:38:41 +0200

    New version. Pipeline test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask on ECS Fargate! 🚀"
+    return "Hello from Flask on ECS Fargate! 🚀 This is a modification which will lead to a new version!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
